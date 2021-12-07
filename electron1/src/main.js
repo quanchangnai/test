3269cfc173ac8e8969eb9bef6add690952a56280
quanchangnai <quanchangnai@126.com>
2021-12-07 19:27:59 +0800

    electron测试

--- electron1/src/main.js
+++ electron1/src/main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require('electron');
+const {app, ipcMain, BrowserWindow} = require('electron');
 
 function createWindow() {
     let window = new BrowserWindow({
@@ -10,7 +10,10 @@ function createWindow() {
         }
     });
 
-    window.loadFile('src/index.html').then(() => console.log('load index.html'))
+    window.loadFile('src/index.html').then(() => {
+        console.log('load index.html');
+        window.webContents.send("init", "aaa");
+    });
 }
 
 app.whenReady().then(createWindow);
@@ -18,4 +21,8 @@ app.whenReady().then(createWindow);
 app.on('window-all-closed', function () {
     console.log('window-all-closed');
     app.quit();
+});
+
+ipcMain.on("test2", (evvent, arg) => {
+    console.log("test2:" + arg);
 });
